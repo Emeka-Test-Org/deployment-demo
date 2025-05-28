17a8d63f1e58e912c4453b79cac5b756ff9e6747
Chukwuemeka Nwaoma <66322582+emekanwaoma@users.noreply.github.com>
2025-05-28 15:16:26 +0100

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -1,12 +1,1 @@
-import os
 
-# BAD: Arbitrary command execution
-user_input = input("Enter your name: ")
-os.system(f"echo Hello {user_input}")
-
-# BAD: Hardcoded credentials
-username = "admin"
-password = "password123"
-
-# BAD: Hardcoded AWS secret
-AWS_SECRET = "AKIA1234567890SECRET"
