11aa1122bf60781608cec52ca3c7c2b822e845c4
Chukwuemeka Nwaoma <66322582+emekanwaoma@users.noreply.github.com>
2025-06-13 16:32:10 +0100

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -1,4 +1,8 @@
-"nothing to see here okk"
+import subprocess
 
 def run_user_code(user_input):
     eval(user_input)
+
+def bad():
+    user_input = input("Run: ")
+    subprocess.call(user_input, shell=True)  # Dangerous: shell injection
