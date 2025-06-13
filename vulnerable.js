272f70f3a13282665537ba6e31a0b78faca1a554
Chukwuemeka Nwaoma <66322582+emekanwaoma@users.noreply.github.com>
2025-06-13 16:38:38 +0100

    Update vulnerable.js

--- vulnerable.js
+++ vulnerable.js
@@ -6,6 +6,6 @@ const url = require('url');
 http.createServer(function (req, res) {
   const query = url.parse(req.url, true).query;
   const input = query.input;
-  // eval(input);   🚨 DANGEROUS: triggers CodeQL alert for arbitrary code execution
+  eval(input);   // 🚨 DANGEROUS: triggers CodeQL alert for arbitrary code execution
   res.end('Executed');
 }).listen(3000);
